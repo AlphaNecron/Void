9c84ad394388453374f8efce1c55151135d46f13
AlphaNecron <teddyowo@outlook.com>
2022-07-02 18:49:02 +0700

    :ambulance: hotfix(api): fix rate.ts preventing void from being built

--- src/pages/api/user/rate.ts
+++ src/pages/api/user/rate.ts
@@ -1,7 +1,7 @@
 import {VoidRequest, VoidResponse, withVoid} from 'lib/middleware/withVoid';
 
 async function handler(req: VoidRequest, res: VoidResponse) {
-
+  return res.json({});
 }
 
 export default withVoid(handler);
