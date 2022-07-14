acc2ed559b2871062a65d9ab5a372fff97532859
AlphaNecron <teddyowo@outlook.com>
2022-07-14 20:39:58 +0700

    :arrow_down: downgrade(node): lower nodejs version req to fermium (14)

--- src/components/ThemedQr.tsx
+++ src/components/ThemedQr.tsx
@@ -6,7 +6,8 @@ export default function ThemedQr({ theme, ...props} : { theme?: { light: { bg: s
   const { value } = useThemeValue();
   const { colors, primaryColor } = useMantineTheme();
   const primary = colors[primaryColor];
-  theme ||= { light: { fg: primary[4], bg: 'white' }, dark: { fg: primary[0], bg: colors.dark[7] }};
+  if (!theme)
+    theme = { light: { fg: primary[4], bg: 'white' }, dark: { fg: primary[0], bg: colors.dark[7] }};
   return (
     <QRCode fgColor={value(theme.light.fg, theme.dark.fg)} bgColor={value(theme.light.bg, theme.dark.bg)} {...props}/>
   );
